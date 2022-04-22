d02553eb981633180410676217255b3f855f6e59
minuk0506 <leeminuk9238@daum.net>
2022-04-22 16:50:08 +0900

    0422spring

---
+++ SpMVC_003_School/src/main/java/com/callor/app/service/StudentService.java
@@ -0,0 +1,15 @@
+package com.callor.app.service;
+
+import java.util.List;
+
+import com.callor.app.domain.StudentVO;
+
+public interface StudentService {
+
+	public int insert(StudentVO studentVO);
+
+	public List<StudentVO> selectAll();
+
+	public StudentVO findById(String stNum);
+
+}
