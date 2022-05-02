8891e0aaf48684deb4016e6724f7dfb105bd662f
minuk0506 <leeminuk9238@daum.net>
2022-05-02 16:04:43 +0900

    0502spring

---
+++ SpMVC_003_SchoolV3/src/main/java/com/callor/school/exec/ExecV1.java
@@ -0,0 +1,33 @@
+package com.callor.school.exec;
+
+public class ExecV1 {
+
+	public static void main(String[] args) {
+		
+		int[] nums = new int[10];
+		for(int i = 0 ; i < nums.length ; i++) {
+			nums[i] = (int)(Math.random() * 100) + 1;
+		}
+		
+		for(int num : nums) {
+		System.out.printf("%d\t", num);
+		}
+		System.out.println();
+		
+		for(int i = 0 ; i < nums.length ; i++) {
+			for(int j = i +1 ; j < nums.length ; j++) {
+				if(nums[i] > nums[j]) {
+					
+					// nums[i] 값과 nums[j] 값을 서로 교환
+					int _t = nums[i]; // _t : 버릴 변수
+					nums[i] = nums[j];
+					nums[j] = _t;
+				}
+			}
+		}
+		for(int num : nums) {
+			System.out.printf("%d\t", num);
+			}
+	}
+	
+}
---
+++ SpMVC_003_SchoolV3/src/main/java/com/callor/school/exec/ExecV2.java
@@ -0,0 +1,59 @@
+package com.callor.school.exec;
+
+public class ExecV2 {
+
+	public static void main(String[] args) {
+
+		int[] intKor = new int[5];
+		int[] intEng = new int[5];
+		int[] intMath = new int[5];
+		int[] intSum = new int[5];
+		System.out.println("국어\t영어\t수학\t총점");
+		for (int i = 0; i < intKor.length; i++) {
+			int iKor = (int) (Math.random() * 100) + 1;
+			int iEng = (int) (Math.random() * 100) + 1;
+			int iMath = (int) (Math.random() * 100) + 1;
+
+			int iSum = iKor + iEng + iMath;
+
+			intKor[i] = iKor;
+			intEng[i] = iEng;
+			intMath[i] = iMath;
+			intSum[i] = iSum;
+		}
+		printScore(intKor, intEng, intMath, intSum);
+		System.out.println("-".repeat(50));
+
+		for (int i = 0; i < intSum.length; i++) {
+			for (int j = i + 1; j < intSum.length; j++) {
+				if (intSum[i] > intSum[j]) {
+					int _t = intSum[i];
+					intSum[i] = intSum[j];
+					intSum[j] = _t;
+					
+					_t = intKor[i];
+					intKor[i] = intKor[j];
+					intKor[j] = _t;
+					
+					_t = intEng[i];
+					intEng[i] = intEng[j];
+					intEng[j] = _t;
+					
+					_t = intMath[i];
+					intMath[i] = intMath[j];
+					intMath[j] = _t;
+				}
+			}
+		}
+		printScore(intKor, intEng, intMath, intSum);
+	} // end main
+
+	private static void printScore(int[] intKor, int[] intEng, int[] intMath, int[] intSum) {
+		for (int i = 0; i < intKor.length; i++) {
+			System.out.printf("%3d\t", intKor[i]);
+			System.out.printf("%3d\t", intEng[i]);
+			System.out.printf("%3d\t", intMath[i]);
+			System.out.printf("%3d\n", intSum[i]);
+		}
+	}
+}
