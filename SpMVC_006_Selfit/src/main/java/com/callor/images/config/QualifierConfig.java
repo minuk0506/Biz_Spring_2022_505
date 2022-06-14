01961f68bd54e5c6aac68bc36971c7beadb36535
minuk0506 <leeminuk9238@daum.net>
2022-06-14 14:53:23 +0900

    0614

---
+++ SpMVC_006_Selfit/src/main/java/com/callor/images/config/QualifierConfig.java
@@ -0,0 +1,15 @@
+package com.callor.images.config;
+
+public class QualifierConfig {
+/*
+ * 문자열을 변수에 담아 오타없게
+ * 얼마든지 미리 만들어 놓아도 된다
+ */
+	public static class SERVICE{
+		public static final String USER_V1 = "userServiceV1";
+		public static final String USER_V2 = "userServiceV2";
+		
+		public static final String SELFIT_V1 = "selfitServiceV1";
+	}
+	
+}
